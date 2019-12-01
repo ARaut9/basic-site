a48770e220a7482e43e074803cd554520e3e044d
ARaut9 <ankitraut999@gmail.com>
2019-12-01 19:46:40 +0530

    refactor the code using express

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
-const http = require('http');
-const url = require('url');
-const fs = require('fs');
+const express = require('express');
+const app = express();
+const path = require('path');
 
-http.createServer((req, res) => {
-  const reqUrl = url.parse(req.url, true);
-  const pathName = "." + reqUrl.pathname;
-  let fileName;
-  console.log(pathName);
-  if (pathName === './') {
-    fileName = 'index.html';
-  } else {
-    fileName = pathName + ".html";
-  }
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
-  fs.readFile(fileName, (err, data) => {
+app.get('/:filename', (req, res, next) => {
+  const filename = `${req.params.filename}.html`
+  res.sendFile(path.join(__dirname, filename), err => {
     if (err) {
-      fs.readFile("./404.html", (err, data) => {
-        res.writeHead(404, { 'Content-Type': 'text/html' });
-        res.write(data);
-        return res.end();
-      });
-    } else {
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.write(data);
-      return res.end();
-    }
+      next(err);
+    };
   });
-}).listen(8080);
+});
+
+app.use((req, res, next) => {
+  const err = new Error('404 Page Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status);
+  res.sendFile(path.join(__dirname, '404.html'));
+});
+
+app.listen(3000, () => {
+  console.log('server running on port 3000');
+});
